0df16b8dd3cde5f17b269fff5a530ec16c47d72f
Valentin Haenel <valentin.haenel@gmx.de>
2014-04-11 11:15:39 +0200

    why

--- graphite2pandas.py
+++ graphite2pandas.py
@@ -8,6 +8,10 @@ import json
 Author: Valentin Haenel <valentin@haenel.co>
 Licence: WTFPL
 
+Can be used to do fetch and load data for exploratory data analysis (EDA) and
+for prototyping advanced algorithms on your local machine before hacking on
+graphites `functions.py`.
+
 Example
 -------
 
